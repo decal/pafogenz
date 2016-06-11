c0fb17e027852897648d74e00c825fce49fb0cc7
Derek Callaway <decal@sdf.org>
2016-06-11 03:06:11 -0400

    Symbolic links for directories..

--- compile.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-gcc -Wall -ansi -pedantic -std=c11 -g -O0 -o cstrarr cstrarr.c
-
-exit 0
--- src/verbose_exit.c
+++ src/verbose_exit.c
@@ -1,7 +1,7 @@
 #include"pafogenz.h"
 
 void verbose_exit(const char *fn, const int st, const char *fl, const int ln) {
-  fprintf(stderr, "%s:%u %s => %s\n", fl, ln, fn, strerror(errno));
+  fprintf(stderr, "%s:%u %s => %s\n", fl, ln, fn, strerror(st));
 
   exit(st);
 }
